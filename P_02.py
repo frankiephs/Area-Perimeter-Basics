5fc4977ec2d6a30ea3855176a874bd91dc5d05d6
Frankie <132312999+frankiephs@users.noreply.github.com>
2024-02-13 10:10:45 +1300

    Second commit

---
+++ P_02.py
@@ -0,0 +1,14 @@
+# Code learn about variables...
+
+# Variables can be a string (Note the speech marks)
+greeting = "Hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Const are uppercase
+PI = 3.14
+
+a_01_my_num = 7
+
