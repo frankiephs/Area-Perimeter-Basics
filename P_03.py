69457d7cceb3cf31974d28ca4204acb6c216f3f1
Frankie <132312999+frankiephs@users.noreply.github.com>
2024-02-13 10:22:30 +1300

    third file

---
+++ P_03.py
@@ -0,0 +1,19 @@
+username = input('What is your name')
+
+fav_num = int(input("What is your fav num"))
+
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+
+print("Hi " + username + ", your fav num is " + str(fav_num))
+
+fav_num = str(fav_num)
+print("Double " + fav_num + " is " + str(double))
+print("half " + fav_num + " is " + str(double))
+print("squared " + fav_num + " is " + str(double))
+print()
+print("Have a nice day")
+
+# there are changes on the syntax because the python ver is outdated on this computer, format func and adding string and int in print also gives an error
